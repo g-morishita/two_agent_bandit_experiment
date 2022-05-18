84a445f96d509df4fb779b2440d9085c5e20884f
g-morishita <optimistic.gota@gmail.com>
2022-05-18 14:11:29 +1000

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {initJsPsych} from 'jspsych';
-import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
-import htmlButtonResponse from '@jspsych/plugin-html-button-response';
+import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response'; import htmlButtonResponse from '@jspsych/plugin-html-button-response';
+import jsPsychPreload from '@jspsych/plugin-preload';
 import {QLearner} from './agents.js'
 import {StableBernoulliBandit} from './bandits.js'
 import {config} from './config.js'
@@ -9,55 +9,61 @@ const jsPsych = initJsPsych({
   on_finish: () => jsPsych.data.displayData() // for debugging
 });
 const timeline = [];
+
+// Instantiate an agent class.
 const agent = new QLearner(
   config.agentAlpha,
   config.agentBeta,
   config.rewardDist.length
 );
-let rewardDist = config.rewardDist;
+
 // Shuffule the reward distribution.
+let rewardDist = config.rewardDist;
 const randomInt = Math.floor(Math.random() * 2);
 [rewardDist[1], rewardDist[0]] = [rewardDist[randomInt], rewardDist[1 - randomInt]];
 
 const bandit = new StableBernoulliBandit(rewardDist);
 
+// Preload the images.
+const preload = {
+  type: jsPsychPreload,
+  auto_preload: true,
+  images: ["img/blue_slot_machine.png", "img/red_slot_machine.png", "img/coin_medal_silver.png"]
+}
+timeline.push(preload)
 
 const welcome = {
   type: htmlKeyboardResponse,
   stimulus: `<h1>Welcome to the experiment. Press any key to begin.</h1>`
 };
-
 timeline.push(welcome);
 
-// TODO: Need to modify the experiment
 const instructions = {
   type: htmlKeyboardResponse,
   stimulus: `
-    <p>In this experiment, a circle will appear in the center 
-    of the screen.</p><p>If the circle is <strong>blue</strong>, 
-    press the letter F on the keyboard as fast as you can.</p>
-    <p>If the circle is <strong>orange</strong>, press the letter J 
-    as fast as you can.</p>
-    <div style='width: 700px;'>
-    <div style='float: left;'><img src='img/blue.png'></img>
-    <p class='small'><strong>Press the F key</strong></p></div>
-    <div style='float: right;'><img src='img/orange.png'></img>
-    <p class='small'><strong>Press the J key</strong></p></div>
+    <p>in this experiment, there are two slot machines with different colors, red and blue.
+    In each trial, you are going to choose one of them by clicking the image of the slot and know if you win or not later.</p>
+    <p>Each of these two slots have a different probability of winning. Hence, you should "explore" and know which slot is btter.</p>
+    <p>Note that the probabilies are fixed through the same session.</p>
+    <p>Also, there is another person who play the same slot machines. You can observe his/her result after each trial while the other also observe your own result.</p>
+    <div style='width: 800px;'>
+    <div style='float: left;'><img src='img/blue_slot_machine.png'></img>
+    <div style='float: right;'><img src='img/red_slot_machine.png'></img>
     </div>
-    <p>Press any key to begin.</p>
+    <p>press any key to begin.</p>
   `,
   on_finish: (data) => {
     data.rewardDist = rewardDist;
     data.config = config;
   }
 };
-
 timeline.push(instructions);
 
 const trial = {
   type: htmlButtonResponse,
   stimulus: "",
-  choices: ["arm1", "arm2"],
+  choices: ["red_slot_machine.png", "blue_slot_machine.png"],
+  button_html: `<img src='img/%choice%'></img>`,
   on_finish: (data) => {
     const chosenArm = parseInt(data.response);
     data.arm = chosenArm;
@@ -78,9 +84,15 @@ const ownResult = {
   type: htmlKeyboardResponse,
   stimulus: () => {
     const data =  jsPsych.data.get().last(1).values()[0];
-    const arm = data["arm"];
+    const armColor = data["arm"] == 0 ? "red" : "blue";
     const reward = data["reward"];
-    return `You chose Arm ${arm}. You got a reward of ${reward}`;
+    let html = `<p>You chose <font color="${armColor}">${armColor}</font>.</p>`
+    if (reward == 1) {
+      html += `<div style="width: 600px"><img src="img/coin_medal_silver.png"></img></div>`
+    } else {
+      html += "You failed..." // TODO: Add some image for a failure.
+    }
+    return html;
   }
 };
 
@@ -88,9 +100,15 @@ const agentResult = {
   type: htmlKeyboardResponse,
   stimulus: () => {
     const data =  jsPsych.data.get().last(2).values()[0];
-    const arm = data["agentArm"];
+    const armColor = data["agentArm"] == 0 ? "red" : "blue";
     const reward = data["agentReward"];
-    return `The other chose Arm ${arm}. The other got a reward of ${reward}`;
+    let html = `<p>The other chose <font color="${armColor}">${armColor}</font>.</p>`
+    if (reward == 1) {
+      html += `<div style="width: 600px"><img src="img/coin_medal_silver.png"></img></div>`
+    } else {
+      html += "The other failed..." // TODO: Add some image for a failure.
+    }
+    return html;
   }
 }
 
