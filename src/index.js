0481d83c688df5fd83c6a2dc1ab6dd34a7b36d4e
g-morishita <optimistic.gota@gmail.com>
2022-05-18 12:14:57 +1000

    implement two agent experiemnt

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 import {initJsPsych} from 'jspsych';
 import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response';
 import htmlButtonResponse from '@jspsych/plugin-html-button-response';
-// import imageKeyboardResponse from '@jspsych/plugin-image-keyboard-response';
-// import preload from '@jspsych/plugin-preload';
+import {QLearner} from './agents.js'
+import {StableBernoulliBandit} from './bandits.js'
+import {config} from './config.js'
+
 
 const jsPsych = initJsPsych({
   on_finish: () => jsPsych.data.displayData() // for debugging
 });
-
 const timeline = [];
+const agent = new QLearner(0.1, 2, 2);
+const bandit = new StableBernoulliBandit([0.4, 0.7]);
+
 
 const welcome = {
   type: htmlKeyboardResponse,
-  stimulus: "<h1>Welcome to the experiment. Press any key to begin.</h1>"
+  stimulus: `<h1>Welcome to the experiment. Press any key to begin.</h1>`
 };
 
 timeline.push(welcome);
 
+// TODO: Need to modify the experiment
 const instructions = {
     type: htmlKeyboardResponse,
     stimulus: `
@@ -33,7 +38,7 @@ const instructions = {
       </div>
       <p>Press any key to begin.</p>
     `,
-    post_trial_gap: 2000
+    post_trial_gap: 200
 };
 
 timeline.push(instructions);
@@ -41,9 +46,48 @@ timeline.push(instructions);
 const trial = {
   type: htmlButtonResponse,
   stimulus: "",
-  choices: ["arm1", "arm2"]
+  choices: ["arm1", "arm2"],
+  on_finish: (data) => {
+    const chosenArm = parseInt(data.response);
+    data.arm = chosenArm;
+    data.reward = bandit.getReward(chosenArm);
+
+    // Agent's trial
+    const agentChosenArm = agent.takeAction();
+    const agentReward = bandit.getReward(chosenArm);
+    data.agentArm = agentChosenArm;
+    data.agentReward = agentReward;
+
+    // Agent's update based on your own experiment
+    agent.updateQValues(agentChosenArm, agentReward);
+  }
 }
 
-timeline.push(trial)
+const ownResult = {
+  type: htmlKeyboardResponse,
+  stimulus: () => {
+    const data =  jsPsych.data.get().last(1).values()[0];
+    const arm = data["arm"];
+    const reward = data["reward"];
+    return `You chose Arm ${arm}. You got a reward of ${reward}`;
+  }
+};
+
+const agentResult = {
+  type: htmlKeyboardResponse,
+  stimulus: () => {
+    const data =  jsPsych.data.get().last(2).values()[0];
+    const arm = data["agentArm"];
+    const reward = data["agentReward"];
+    return `The other chose Arm ${arm}. The other got a reward of ${reward}`;
+  }
+}
+
+const test_procedure = {
+  timeline: [trial, ownResult, agentResult],
+  repetitions: config["timeHorizon"]
+};
+
+timeline.push(test_procedure);
 
 jsPsych.run(timeline);
