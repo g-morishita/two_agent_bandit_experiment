6c13dc2757560d9b657d22d11d5b7721c6e67e6d
g-morishita <optimistic.gota@gmail.com>
2022-05-18 12:39:16 +1000

    add config

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const config = {
+  agentAlpha: 0.1,
+  agentBeta: 2,
+  rewardDist: [0.4, 0.7],
+  timeHorizon: 10
+}
--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import {QLearner} from './agents.js'
 import {StableBernoulliBandit} from './bandits.js'
 import {config} from './config.js'
 
-
 const jsPsych = initJsPsych({
   on_finish: () => jsPsych.data.displayData() // for debugging
 });
 const timeline = [];
-const agent = new QLearner(0.1, 2, 2);
-const bandit = new StableBernoulliBandit([0.4, 0.7]);
+const agent = new QLearner(
+  config.agentAlpha,
+  config.agentBeta,
+  config.rewardDist.length
+);
+let rewardDist = config.rewardDist;
+const randomInt = Math.floor(Math.random());
+rewardDist[1], rewardDist[0] = rewardDist[randomInt], rewardDist[1 - randomInt];
+const bandit = new StableBernoulliBandit(rewardDist);
 
 
 const welcome = {
@@ -23,22 +29,25 @@ timeline.push(welcome);
 
 // TODO: Need to modify the experiment
 const instructions = {
-    type: htmlKeyboardResponse,
-    stimulus: `
-      <p>In this experiment, a circle will appear in the center 
-      of the screen.</p><p>If the circle is <strong>blue</strong>, 
-      press the letter F on the keyboard as fast as you can.</p>
-      <p>If the circle is <strong>orange</strong>, press the letter J 
-      as fast as you can.</p>
-      <div style='width: 700px;'>
-      <div style='float: left;'><img src='img/blue.png'></img>
-      <p class='small'><strong>Press the F key</strong></p></div>
-      <div style='float: right;'><img src='img/orange.png'></img>
-      <p class='small'><strong>Press the J key</strong></p></div>
-      </div>
-      <p>Press any key to begin.</p>
-    `,
-    post_trial_gap: 200
+  type: htmlKeyboardResponse,
+  stimulus: `
+    <p>In this experiment, a circle will appear in the center 
+    of the screen.</p><p>If the circle is <strong>blue</strong>, 
+    press the letter F on the keyboard as fast as you can.</p>
+    <p>If the circle is <strong>orange</strong>, press the letter J 
+    as fast as you can.</p>
+    <div style='width: 700px;'>
+    <div style='float: left;'><img src='img/blue.png'></img>
+    <p class='small'><strong>Press the F key</strong></p></div>
+    <div style='float: right;'><img src='img/orange.png'></img>
+    <p class='small'><strong>Press the J key</strong></p></div>
+    </div>
+    <p>Press any key to begin.</p>
+  `,
+  on_finish: (data) => {
+    data.rewardDist = rewardDist;
+    data.config = config;
+  }
 };
 
 timeline.push(instructions);
